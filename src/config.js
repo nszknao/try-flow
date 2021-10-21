21534bb1e6a484e3c5898d376ebd437b7628860c
Naoto Nishizaka <nszk.nao@gmail.com>
2021-10-21 16:49:41 +0900

    tutorial途中

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ import { config } from "@onflow/fcl";
 
 config({
   "accessNode.api": "https://access-testnet.onflow.org",
-  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "0xProfile": process.env.NEXT_PUBLIC_PROFILE_CONTRACT,
 });
---
+++ src/pages/index.tsx
@@ -0,0 +1,77 @@
+import * as fcl from "@onflow/fcl";
+import React, { useEffect, useState, VFC } from "react";
+
+export const Index: VFC = () => {
+  const [user, setUser] = useState({ loggedIn: null });
+  const [name, setName] = useState<string>();
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)],
+    });
+
+    setName(profile?.name ?? "No Profile");
+  };
+
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction {
+          prepare(account: AuthAccount) {
+            // Only initialize the account if it hasn't already been initialized
+            if (!Profile.check(account.address)) {
+              // This creates and stores the profile in the user's account
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+              // This creates the public capability that lets applications read the profile's info
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+            }
+          }
+        }
+      `,
+      limit: 50,
+    });
+
+    const transaction = await fcl.tx(transactionId).onceSealed();
+    console.log(transaction);
+  };
+
+  const AuthedState = () => {
+    return (
+      <div>
+        <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        <button onClick={sendQuery}>Send Query</button>
+        <button onClick={initAccount}>Init Account</button>
+        <button onClick={fcl.unauthenticate}>Log Out</button>
+      </div>
+    );
+  };
+
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <h1>Flow App</h1>
+      {user.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
+    </div>
+  );
+};
